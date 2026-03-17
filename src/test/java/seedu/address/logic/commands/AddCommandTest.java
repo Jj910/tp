0a975be6af98ba7ca8e7ddd998c7faa48f38f8bd
hwangjihun <e0959872@u.nus.edu>
2026-03-17 15:56:41 +0800

    Fix codestyle error for AddCommandTest

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -50,7 +50,8 @@ public class AddCommandTest {
         AddCommand addCommand = new AddCommand(validGamer);
         ModelStub modelStub = new ModelStubWithGamer(validGamer);
 
-        assertThrows(CommandException.class, AddCommand.MESSAGE_DUPLICATE_GAMERTAG, () -> addCommand.execute(modelStub));
+        assertThrows(CommandException.class,
+                AddCommand.MESSAGE_DUPLICATE_GAMERTAG, () -> addCommand.execute(modelStub));
     }
 
     @Test
